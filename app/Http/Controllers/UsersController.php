37806a1ce82f427df251133ac2270bc2858e4f39
cryptohacks09 <69419109+cryptohacks09@users.noreply.github.com>
2023-04-18 13:32:34 +0500

    updated for clean code review

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,106 +1,47 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\User;
-
-
-class UsersController extends Controller
+public function searchUsers(Request $request)
 {
-    public function index(Request $request)
-{
-    $query = User::query();
+    $userQuery = User::query();
     
-    // User based on age
-    if ($request->filled('age')) {
-        $query->where('age', $request->age);
-    }
-    // Users based on location
-    if ($request->filled('location_id')) {
-        $query->where('location_id', $request->location_id);
-    }
-   //  Users based on Relationships
-    if ($request->filled('relationship_id')) {
-        $query->where('relationship_id', $request->relationship_id);
-    }
-    // Users based on Personalities
-    if ($request->filled('personality_id')) {
-        $query->where('personality_id', $request->personality_id);
-    }
-   //  Users based on Language Proefficiency
+    // Filter users based on age
+    $this->applyFilter($userQuery, 'age', $request->input('age'));
     
-    if ($request->filled('language_id')) {
-        $query->where('language_id', $request->language_id);
-    }
-    //  Users based on Religion
-    if ($request->filled('religion_id')) {
-        $query->where('religion_id', $request->religion_id);
-    }
-    //  Users based on Language Diet wishlist
+    // Filter users based on location
+    $this->applyFilter($userQuery, 'location_id', $request->input('location_id'));
     
-    if ($request->filled('dietary_id')) {
-        $query->where('dietary_id', $request->dietary_id);
-    }
-    //  Users based on Types of Allergies 
-
-    if ($request->filled('allergy_id')) {
-        $query->where('allergy_id', $request->allergy_id);
-    }
-    //  Users  Collection based on above inputs
+    // Filter users based on relationship
+    $this->applyFilter($userQuery, 'relationship_id', $request->input('relationship_id'));
     
-    $users = $query->get();
+    // Filter users based on personality
+    $this->applyFilter($userQuery, 'personality_id', $request->input('personality_id'));
     
+    // Filter users based on language proficiency
+    $this->applyFilter($userQuery, 'language_id', $request->input('language_id'));
+    
+    // Filter users based on religion
+    $this->applyFilter($userQuery, 'religion_id', $request->input('religion_id'));
+    
+    // Filter users based on dietary preferences
+    $this->applyFilter($userQuery, 'dietary_id', $request->input('dietary_id'));
+    
+    // Filter users based on allergies
+    $this->applyFilter($userQuery, 'allergy_id', $request->input('allergy_id'));
+    
+    // Retrieve the filtered users and return as a JSON response
+    $users = $userQuery->get();
     return response()->json($users);
 }
 
-
-public function UsingBigQuery(Request $request)
-    {
-        $bigQuery = new BigQueryClient([
-            'projectId' => 'your-project-id',
-            'keyFile' => json_decode(file_get_contents('/path/to/your/keyfile.json'), true),
-        ]);
-        
-        $query = $bigQuery->query()
-            ->select('*')
-            ->from('your-dataset.users');
-            
-        if ($request->has('age')) {
-            $query->where('age', '=', $request->age);
-        }
-        
-        if ($request->has('location_id')) {
-           $query->where('location_id', '=', $request->location_id);
-        }
-        
-        if ($request->has('relationship_id')) {
-            $query->where('relationship_id', '=', $request->relationship_id);
-        }
-        
-        if ($request->has('personality_id')) {
-            $query->where('personality_id', '=', $request->personality_id);
-        }
-        
-        if ($request->has('language_id')) {
-            $query->where('language_id', '=', $request->language_id);
-        }
-        
-        if ($request->has('religion_id')) {
-            $query->where('religion_id', '=', $request->religion_id);
-        }
-        
-        if ($request->has('dietary_id')) {
-            $query->where('dietary_id', '=', $request->dietary_id);
-        }
-        
-        if ($request->has('allergy_id')) {
-            $query->where('allergy_id', '=', $request->allergy_id);
-        }
-        
-        $results = $query->execute();
-        
-        return response()->json($results->rows());
+/**
+ * Apply a filter to the user query if the filter value is not empty.
+ *
+ * @param \Illuminate\Database\Eloquent\Builder $query
+ * @param string $filterName
+ * @param mixed $filterValue
+ * @return void
+ */
+private function applyFilter($query, $filterName, $filterValue)
+{
+    if (!empty($filterValue)) {
+        $query->where($filterName, $filterValue);
     }
-
 }
